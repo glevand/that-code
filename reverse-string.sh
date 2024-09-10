73bc3c200d3088f082695da1f05ed9e5a3769615
Geoff Levand <geoff@infradead.org>
2024-09-10 20:22:14 +0900

    fu reverse-string.sh
    Signed-off-by: Geoff Levand <geoff@infradead.org>

--- reverse-string.sh
+++ reverse-string.sh
@@ -9,16 +9,18 @@ usage() {
 		echo "${script_name} - Reverse input string."
 		echo "Usage: ${script_name} [flags] <input string>"
 		echo "Option flags:"
+		echo "  -t --test       - Run tests. Default: '${do_tests}'."
 		echo "  -h --help       - Show this help and exit."
 		echo "  -v --verbose    - Verbose execution."
 		echo "  -g --debug      - Extra verbose execution."
+		echo "Input string      = '${input_str}'"
 	} >&2
 	eval "${old_xtrace}"
 }
 
 process_opts() {
-	local short_opts="hvg"
-	local long_opts="help,verbose,debug"
+	local short_opts="thvg"
+	local long_opts="test,help,verbose,debug"
 
 	local opts
 	opts=$(getopt --options ${short_opts} --long ${long_opts} -n "${script_name}" -- "$@")
@@ -28,6 +30,10 @@ process_opts() {
 	while true ; do
 		# echo "${FUNCNAME[0]}: (${#}) '${*}'"
 		case "${1}" in
+		-t | --test)
+			do_tests=1
+			shift
+			;;
 		-h | --help)
 			usage=1
 			shift
@@ -44,8 +50,8 @@ process_opts() {
 			;;
 		--)
 			shift
-			input_str="${1:-}"
-			if [[ ${input_str} ]]; then
+			if [[ "${1:-}" ]]; then
+				input_str="${1:-}"
 				shift
 			fi
 			extra_args="${*}"
@@ -57,16 +63,6 @@ process_opts() {
 			;;
 		esac
 	done
-}
-
-check_program() {
-	local prog="${1}"
-	local path="${2}"
-
-	if ! test -x "$(command -v "${path}")"; then
-		echo "${script_name}: ERROR: Please install '${prog}'." >&2
-		exit 1
-	fi
 }
 
 on_exit() {
@@ -83,11 +79,69 @@ on_err() {
 	local line_no=${2}
 	local err_no=${3}
 
-	keep_tmp_dir=1
-
 	echo "${script_name}: ERROR: function=${f_name}, line=${line_no}, result=${err_no}" >&2
 
 	exit "${err_no}"
+}
+
+reverse_string() {
+	local input_str="${1}"
+	local input_len=${#input_str}
+	local output_str=''
+	local i
+
+	for (( i = input_len - 1; i >= 0; i-- )); do
+		output_str="${output_str}${input_str:${i}:1}"
+# 		echo "output_str = '${output_str}'" >&2
+	done
+
+	echo "${output_str}"
+}
+
+run_tests() {
+	local input_str=''
+	verbose=1
+
+	echo "=== ${FUNCNAME[0]} start ===" >&2
+
+	input_str='aabb12.5+55.3ccdd23eeffeee45.6hyhj '
+	output_str="$(reverse_string "${input_str}")"
+	echo "${FUNCNAME[0]}: input_str  = '${input_str}'" >&2
+	echo "${FUNCNAME[0]}: expected   = ' jhyh6.54eeeffee32ddcc3.55+5.21bbaa'" >&2
+	echo "${FUNCNAME[0]}: output_str = '${output_str}'" >&2
+
+	input_str='65'
+	output_str="$(reverse_string "${input_str}")"
+	echo "${FUNCNAME[0]}: input_str  = '${input_str}'" >&2
+	echo "${FUNCNAME[0]}: expected   = '56'" >&2
+	echo "${FUNCNAME[0]}: output_str = '${output_str}'" >&2
+
+	input_str='eeeeeeeeeeeeeeeeeeeEeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee4rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr6'
+	output_str="$(reverse_string "${input_str}")"
+	echo "${FUNCNAME[0]}: input_str  = '${input_str}'" >&2
+	echo "${FUNCNAME[0]}: expected   = '6rrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr4eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeEeeeeeeeeeeeeeeeeeee'" >&2
+	echo "${FUNCNAME[0]}: output_str = '${output_str}'" >&2
+
+	input_str='77.777777777777777ft4gfgggfd+6.88888888888hhh'
+	output_str="$(reverse_string "${input_str}")"
+	echo "${FUNCNAME[0]}: input_str  = '${input_str}'" >&2
+	echo "${FUNCNAME[0]}: expected   = 'hhh88888888888.6+dfgggfg4tf777777777777777.77'" >&2
+	echo "${FUNCNAME[0]}: output_str = '${output_str}'" >&2
+
+	input_str='-77.7a6.8'
+	output_str="$(reverse_string "${input_str}")"
+	echo "${FUNCNAME[0]}: input_str  = '${input_str}'" >&2
+	echo "${FUNCNAME[0]}: expected   = '8.6a7.77-'" >&2
+	echo "${FUNCNAME[0]}: output_str = '${output_str}'" >&2
+
+	input_str='aabb -12.5 ccdd+23-66.6eeffeee 45.6 hyhj'
+	output_str="$(reverse_string "${input_str}")"
+	echo "${FUNCNAME[0]}: input_str  = '${input_str}'" >&2
+	echo "${FUNCNAME[0]}: expected   = 'jhyh 6.54 eeeffee6.66-32+ddcc 5.21- bbaa'" >&2
+	echo "${FUNCNAME[0]}: output_str = '${output_str}'" >&2
+
+	echo "=== ${FUNCNAME[0]} end ===" >&2
+	echo >&2
 }
 
 #===============================================================================
@@ -109,9 +163,12 @@ set -eE
 set -o pipefail
 set -o nounset
 
+do_tests=''
 usage=''
 verbose=''
 debug=''
+input_str=''
+output_str=''
 
 process_opts "${@}"
 
@@ -128,22 +185,23 @@ if [[ ${extra_args} ]]; then
 	exit 1
 fi
 
-rev="${rev:-rev}"
-check_program "rev" "${rev}"
-
-if [[ ! ${input_str} ]]; then
-	echo "${script_name}: ERROR: No input string given." >&2
-	usage
-	exit 1
+if [[ ${verbose} ]]; then
+	echo "${script_name}" >&2
 fi
 
-output_str=$(echo "${input_str}" | ${rev})
-
-if [[ ${verbose} ]]; then
-	echo "${script_name}: Input string  = '${input_str}'." >&2
-	echo "${script_name}: Output string = '${output_str}'." >&2
+if [[ ${do_tests} ]]; then
+	run_tests
 else
-	echo "${output_str}"
+	if [[ ! ${input_str} ]]; then
+		echo "${script_name}: ERROR: No input string given." >&2
+		usage
+		exit 1
+	fi
+
+	output_str="$(reverse_string "${input_str}")"
+
+	echo "${script_name}: Input string  = '${input_str}'" >&2
+	echo "${script_name}: Output string = '${output_str}'" >&2
 fi
 
 trap "on_exit 'Success'" EXIT
